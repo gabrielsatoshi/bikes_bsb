5b756344f52de390c2d7309f4dd3a67981fe5dc7
l1nu$ <112598996+gabrielsatoshi@users.noreply.github.com>
2023-11-14 17:15:44 -0300

    Add files via upload

---
+++ projeto_bike/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+import sqlite3
+
+app = FastAPI()
+
+
+def conectar_banco():
+    return sqlite3.connect("banco_bikes")
+@app.get("/df_stations")
+def pegar_df_stations():
+    con = conectar_banco()
+    cursor = con.cursor()
+    query = 'select * from df_stations'
+    cursor.execute(query)
+    dados = cursor.fetchall()
+    con.close()
+    return {"Dados df_stations :":dados}
+
+@app.get("/df_rides")
+def pegar_df_stations():
+    con = conectar_banco()
+    cursor = con.cursor()
+    query = 'select * from df_rides'
+    cursor.execute(query)
+    dados = cursor.fetchall()
+    con.close()
+
+    return {"Dados df_rides :":dados}
